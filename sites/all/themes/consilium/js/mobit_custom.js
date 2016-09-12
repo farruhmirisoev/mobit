60ec2683dd927d8cba97853357cbed45a228db5c
farruh Mirisoev <farruh.mirisoev@gmail.com>
2016-09-13 04:00:21 +0500

    phone responsive

--- sites/all/themes/consilium/js/mobit_custom.js
+++ sites/all/themes/consilium/js/mobit_custom.js
@@ -5,7 +5,14 @@ jQuery(document).ready(function($) {
     });
     // remove btn class 
     $('#block-webform-client-block-89 .form-submit').removeClass('btn');
+    $('#block-block-72 .mobit_subject__item').click(function() {
+        var slide_index = $(this).index();
+        // if ($('#block-md-slider-1 .md-bullet').length <= slide_index) {
+            $('#block-md-slider-1 .md-bullets .md-bullet').eq(slide_index).click().addClass('test');
+            $('#block-block-72 .mobit_subject__item').removeClass('mobit_subject__item--active');
+            $(this).addClass('mobit_subject__item--active');        	
+        // }
 
+    });
 
 });
-
