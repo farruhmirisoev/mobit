913b5b5049e2925eda0ea75af1722bd14eb91291
farruh Mirisoev <farruh.mirisoev@gmail.com>
2016-09-13 20:39:08 +0500

    якоря

--- sites/all/themes/consilium/js/mobit_custom.js
+++ sites/all/themes/consilium/js/mobit_custom.js
@@ -8,11 +8,35 @@ jQuery(document).ready(function($) {
     $('#block-block-72 .mobit_subject__item').click(function() {
         var slide_index = $(this).index();
         // if ($('#block-md-slider-1 .md-bullet').length <= slide_index) {
-            $('#block-md-slider-1 .md-bullets .md-bullet').eq(slide_index).click().addClass('test');
-            $('#block-block-72 .mobit_subject__item').removeClass('mobit_subject__item--active');
-            $(this).addClass('mobit_subject__item--active');        	
+        $('#block-md-slider-1 .md-bullets .md-bullet').eq(slide_index).click().addClass('test');
+        $('#block-block-72 .mobit_subject__item').removeClass('mobit_subject__item--active');
+        $(this).addClass('mobit_subject__item--active');
         // }
-
+        return false;
     });
 
+    $('#block-superhero-dropdown-superhero-dropdown-block-2 .menu a').click(function() {
+        var linkHash = $(this).prop("hash");
+        console.log(linkHash);
+        scrollToMenu(linkHash);
+        return false;
+    });
+    var pageMap  = new Object();
+    for (var i = 0; i < $('#block-superhero-dropdown-superhero-dropdown-block-2 .menu li').length; i++) {
+    	thislinkHash = $(this).find(a).prop('hash');
+
+    }
+
+
 });
+
+function scrollToMenu(dest) {
+	var stickyHeaderHeight = 0;
+	if($('#section-header.fixed'))
+		stickyHeaderHeight = $('#section-header.fixed').height();	
+
+    $('html, body').animate({
+        scrollTop: $(dest).offset().top - stickyHeaderHeight,
+    }, 700);
+}
+
