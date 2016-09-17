86982d15749723f344f6a97f2a13408a0dfed4ed
farruh Mirisoev <farruh.mirisoev@gmail.com>
2016-09-17 19:25:25 +0500

    main menu highlight on active block while scrolling

--- sites/all/themes/consilium/js/mobit_custom.js
+++ sites/all/themes/consilium/js/mobit_custom.js
@@ -1,6 +1,6 @@
 jQuery(document).ready(function($) {
 
-    // anchor scroll to block
+    //paginator=====================
     $('.mobit_subject__wrap').eq(0).find('.mobit_subject__item').addClass('mobit_subject__item--active');
     for (var i = 0; i < $('#block-md-slider-1 .md-bullets .md-bullet').length; i++) {
         $('.mobit_subject__wrap').eq(i).find('.mobit_subject__item').addClass('mobit_subject__item--attached');
@@ -16,6 +16,8 @@ jQuery(document).ready(function($) {
         }
         return false;
     });
+
+    // anchor scroll to block================
     $('#block-superhero-dropdown-superhero-dropdown-block-2 .menu a').removeClass('active');
     $('#block-superhero-dropdown-superhero-dropdown-block-2 .menu a').click(function() {
         var linkHash = $(this).prop("hash");
@@ -25,70 +27,103 @@ jQuery(document).ready(function($) {
         scrollToMenu(linkHash);
         return false;
     });
-    var pageMap = new Object();
-    for (var i = 0; i < $('#block-superhero-dropdown-superhero-dropdown-block-2 .menu li').length; i++) {
-        thislinkHash = $(this).find('a').prop('hash');
+    //block waypoints
+    var blockWayPointMap = new Object();
+    var mainMenuItemList = $('#block-superhero-dropdown-superhero-dropdown-block-2 .menu li');
+    for (var i = 0; i < mainMenuItemList.length; i++) {
+        thislinkHash = mainMenuItemList.eq(i).find('a').prop('hash');
+        blockWayPointMap[i] = {
+            'offsetTop': $(thislinkHash).offset().top - stickyHeaderHeight(),
+            'offsetBottom': $(thislinkHash).outerHeight() + $(thislinkHash).offset().top - stickyHeaderHeight()
+        }
+    }
+    console.log(blockWayPointMap);
+
+    $(window).scroll(function() {
+        var activeAnchor = false;
+        for (var linkIndex in blockWayPointMap) {
+            if ($(this).scrollTop() >= blockWayPointMap[linkIndex].offsetTop && $(this).scrollTop() <= blockWayPointMap[linkIndex].offsetBottom) {
+                mainMenuItemList.each(function() {
+                    $(this).find('a').removeClass('active-hash');
+                });
+                mainMenuItemList.eq(linkIndex).find('a').addClass('active-hash');
+                activeAnchor =  true;
+                break;
+            }
+        }
+        if (!activeAnchor) {
+            mainMenuItemList.each(function() {
+                $(this).find('a').removeClass('active-hash');
+            });
+        }
+    });
+
+
+
+
+    //block step by step apearance==================
+
+    if ($(this).width() > 600 && $('.ctrl_proccess__step--4-mobile').css('display') == 'none') {
+
+        $step1 = $('.ctrl_proccess__step--1');
+        $step2 = $('.ctrl_proccess__step--2');
+        $step3 = $('.ctrl_proccess__step--3'); //actually it's 4th step, but in layout order
+        $step4 = $('.ctrl_proccess__step--4');
+        $arrowRight = $('.ctrl_proccess__step--arrow-right');
+        $arrowLeft = $('.ctrl_proccess__step--arrow-left');
+        $arrowUp = $('.ctrl_proccess__step--arrow-up');
+        $arrowDown = $('.ctrl_proccess__step--arrow-down');
+        $step1.css('opacity', 0);
+        $step2.css('opacity', 0);
+        $step3.css('opacity', 0);
+        $step4.css('opacity', 0);
+        $arrowRight.css('opacity', 0);
+        $arrowLeft.css('opacity', 0);
+        $arrowUp.css('background-position-y', '-9999px');
+        $arrowDown.css('background-position-y', '-9999px');
+
+        $(window).scroll(function() {
+
+            if ($(this).scrollTop() >= ($step1.offset().top - $(window).height() / 2)) {
+                $step1.fadeTo('slow', 1);
+            }
+            if ($(this).scrollTop() >= ($arrowRight.offset().top - $(window).height() / 2 + 100)) {
+                $arrowRight.fadeTo('slow', 1);
+                $step2.fadeTo('slow', 1);
+            }
+
+            if ($(this).scrollTop() >= ($step3.offset().top - $(window).height() / 2)) {
+                $arrowDown.css('background-position-y', 'bottom');
+                $step4.fadeTo('slow', 1);
+            }
+            if ($(this).scrollTop() >= ($step4.offset().top - $(window).height() / 2 + 100)) {
+                $arrowLeft.fadeTo('slow', 1);
+                $step3.fadeTo('slow', 1);
+                $arrowUp.css('background-position-y', 'top');
+            }
+
+        });
 
     }
-    // $(window).resize(function(){
 
-	    if ($(this).width() > 600 && $('.ctrl_proccess__step--4-mobile').css('display') == 'none') {
-	    	
-	        $step1 = $('.ctrl_proccess__step--1');
-	        $step2 = $('.ctrl_proccess__step--2');
-	        $step3 = $('.ctrl_proccess__step--3'); //actually it's 4th step, but in layout order
-	        $step4 = $('.ctrl_proccess__step--4');
-	        $arrowRight = $('.ctrl_proccess__step--arrow-right');
-	        $arrowLeft = $('.ctrl_proccess__step--arrow-left');
-	        $arrowUp = $('.ctrl_proccess__step--arrow-up');
-	        $arrowDown = $('.ctrl_proccess__step--arrow-down');
-	        $step1.css('opacity', 0);
-	        $step2.css('opacity', 0);
-	        $step3.css('opacity', 0);
-	        $step4.css('opacity', 0);
-	        $arrowRight.css('opacity', 0);
-	        $arrowLeft.css('opacity', 0);
-	        $arrowUp.css('background-position-y', '-9999px');
-	        $arrowDown.css('background-position-y', '-9999px');
-
-	        $(window).scroll(function() {
-	        	
-	            if ($(this).scrollTop() >= ($step1.offset().top - $(window).height() / 2)) {
-	                $step1.fadeTo('slow', 1);
-	            }
-	            if ($(this).scrollTop() >= ($arrowRight.offset().top - $(window).height() / 2 + 100)) {
-	                $arrowRight.fadeTo('slow', 1);
-	                $step2.fadeTo('slow', 1);
-	            }
-
-	            if ($(this).scrollTop() >= ($step3.offset().top - $(window).height() / 2)) {
-	                $arrowDown.css('background-position-y', 'bottom');
-	                $step4.fadeTo('slow', 1);
-	            }
-	            if ($(this).scrollTop() >= ($step4.offset().top - $(window).height() / 2 + 100)) {
-	                $arrowLeft.fadeTo('slow', 1);
-	                $step3.fadeTo('slow', 1);
-                    $arrowUp.css('background-position-y', 'top');
-	            }
-	            
-	        });
-
-	    }
-    	
-    // });
+    //block step by step apearance=====END=============
 
 
 });
 
 function scrollToMenu(dest) {
-    var stickyHeaderHeight = 0;
-    if ($('#section-header.fixed'))
-        stickyHeaderHeight = $('#section-header.fixed').height();
-
     $('html, body').animate({
-        scrollTop: $(dest).offset().top - stickyHeaderHeight,
+        scrollTop: $(dest).offset().top - stickyHeaderHeight(),
     }, 700);
 }
+
+function stickyHeaderHeight() {
+    height = 0;
+    if ($('#section-header.fixed'))
+        height = $('#section-header.fixed').height();
+    return height;
+}
+
 
 Drupal.behaviors.mobit = {
     attach: function(context, settings) {
